05cf0f5fe825392131be399f0be12dc309445579
Theresa Crupi <tcrupi1@gmail.com>
2017-03-15 21:09:06 -0400

    created session varibles for lit and preferences on the browse page

--- app/browse/views.py
+++ app/browse/views.py
@@ -2,9 +2,11 @@
 # Browse #
 ##########
 from flask_paginate import Pagination
-from flask import render_template, redirect, url_for, flash, request, make_response
+from flask_session import Session
+from flask import render_template, redirect, url_for, flash, request, make_response, session
 from . import browse
 from .forms import BrowseForm
+from .browseForm import browseForm
 from .. import db
 # Import database model
 from ..models import Lit, LitEditRecord
@@ -19,88 +21,54 @@ default_pref = {"author": True, "yrPublished": True, "title":True, "sourceTitle"
 
 def browse():
 
-        form = BrowseForm()
+    form = BrowseForm()
 
-    	search = False
-    	q = request.args.get('q')
-    	if q:
-    		search = True
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
 
-    	total = Lit.objects.count()
-    	page = request.args.get('page', type=int, default=1)
+    total= Lit.objects.count()
 
-    	#start=page*10-10
-    	#end=page*10
+    # if a POST request is made: get form, lit and preferences returned in browseForm
+    # store lit and preferences in session variables
+    if request.method == 'POST':
+        form, lit, preferences = browseForm(request, form)
+        session['lit'] = lit
+        session['preferences'] = preferences
+        page = request.args.get('page', type=int, default=1)
 
-        #lit = Lit.objects[start:end].order_by('-yrPublished')
+    # if a GET request is made: check if there are lit and preferences sessions, if so use them
+    if request.method == 'GET':
+        if 'lit' in session:
+            lit = session.get('lit')
+        else:
+            lit = Lit.objects.order_by('-yrPublished')
+        if 'preferences' in session:
+            preferences = session.get('preferences')
+        else:
+            preferences = default_pref
+        page = request.args.get('page', type=int, default=1)
+    else:
+        page=1
 
-        ##### lines 39 to 48 correctly sort the db, however going to the next page undoes the sort. must investigate. ####
+    # allowing 30 entries per page
+    i=(page-1)*30
+    lit_page=lit[i:i+30]
 
-        lit=Lit.objects.order_by('-yrPublished')
+    pagination = Pagination(
+        page=page,
+        per_page=30,
+        total=total,
+        search=search,
+        record_name='references'
+    )
 
-        if form.sort.data and str(form.sort.data) != 'None':
-            sortStr = str(form.sort.data)
-            lit = sorted(lit, key=lambda lit:getattr(lit, sortStr))
-
-        i=(page-1)*10
-        lit_page=lit[i:i+10]
-
-    	preferences = request.cookies.get('preferences')
-    	if not preferences:
-     		# Return default preferences
-     		preferences = default_pref
-     	else:
-     		# Otherwise convert the cookie to a python object
-     		preferences = json.loads(preferences)
-
-        pagination = Pagination(
-            page=page,
-    		per_page=10,
-    		total=total,
-    		search=search,
-    		record_name='references'
-    	)
-
-    	return render_template(
-    		'browse.html',
-        	lit = lit_page,
-        	pagination = pagination,
-        	total=total,
-        	preferences = preferences,
-            form = form
-        	)
-
-	search = False
-	q = request.args.get('q')
-	if q:
-		search = True
-
-	total = Lit.objects.count()
-	page = request.args.get('page', type=int, default=1)
-
-	start=page*30-30
-	end=page*30
-	lit = Lit.objects[start:end].order_by('-create_date')
-	pagination = Pagination(
-		page=page,
-		per_page=30,
-		total=total,
-		search=search,
-		record_name='references'
-	)
-
-	preferences = request.cookies.get('preferences')
-	if not preferences:
- 		# Return default preferences
- 		preferences = default_pref
- 	else:
- 		# Otherwise convert the cookie to a python object
- 		preferences = json.loads(preferences)
-
-	return render_template(
-		'browse.html',
-    	lit = lit,
-    	pagination = pagination,
-    	total=total,
-    	preferences = preferences,
-    	)
+    return render_template(
+        'browse.html',
+        lit = lit_page,
+        pagination = pagination,
+        total = total,
+        preferences = preferences,
+        form = form
+    )
